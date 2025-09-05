7f895b50f0d007c77e65234e1f3af1e3cef286cb
Dylan <dylant7192@gmail.com>
2025-09-05 16:51:24 +0100

    Completed  implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Write;
+use clap::Parser;
+use anyhow::{Context, Result};
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(args.target.clone())
+        .with_context(|| format!("Could not read file '{}'", args.target.display()))?;
+    let new_file = std::fs::File::create(args.new.clone())
+        .with_context(|| format!("Could not write file '{}'", args.new.display()))?;
+    for line in content.lines() {
+        for byte in line.bytes() {
+           for _ in 0..byte {
+                write!(&new_file, "diddy ").with_context(|| format!("Could not write file '{}'", args.new.display()))?;
+            }
+            writeln!(&new_file).with_context(|| format!("Could not write file '{}'", args.new.display()))?;
+        }
+        writeln!(&new_file, "\n").with_context(|| format!("Could not write file '{}'", args.new.display()))?;
+    }
+    println!("diddy party at {}", args.new.display());
+    Ok(())
+}
+
+#[derive(Parser)]
+struct Cli {
+    target: std::path::PathBuf,
+    new: std::path::PathBuf,
 }
